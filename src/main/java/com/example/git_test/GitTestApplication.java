3e5e6d97b1055a897fd973271e7111bc968cb73e
parky <parkyg84@naver.com>
2022-10-08 16:44:04 +0900

    Initial commit

---
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -0,0 +1,18 @@
+package com.example.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+
+		System.out.println("abc");
+
+
+
+	}
+
+}
